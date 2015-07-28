719a96a482e3e48347d2579f0c15491c551e0984
Juanfran Correas <jfcorreas@gmail.com>
2015-07-28 23:34:53 +0200

    Initial commit with Travis CI

---
+++ test/hello-spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect,
+    should = require('chai').should();
+
+describe('Hello', function () {
+
+    it('should exist', function () {
+      true;
+    });
+
+});
